28f99c57526530e93d8842499c201923d5786d13
samuelloveyou <samuelloveyou@gmail.com>
2017-07-23 13:14:09 +0300

    hello1

---
+++ targil1.js
@@ -0,0 +1,13 @@
+onclick = function() {myFunction()};
+function myFunction() {
+    document.getElementById("nisayon").innerHTML = "YOU CLICKED ME!" ;}
+
+function yourFunction() {
+    document.body.style.backgroundColor = "yellow";
+}
+
+
+function ourFunction() {
+    document.getElementById("change vid").src = "unistudiosglobe.mp4"
+};
+
